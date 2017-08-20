a1e7b3143a5de745ba90c14a4f84b059fca97a97
EMichira <emmanuelmichira@gmail.com>
2017-08-20 22:34:45 +0300

    Added first test to confirm if instatiting is successful

--- src/test/java/SquadTest.java
+++ src/test/java/SquadTest.java
@@ -4,4 +4,11 @@ import static org.junit.Assert.*;
 
 public class SquadTest {
 
+  //asserts if instances can be created succefully
+    @Test
+    public void Squad_instantiatesCorrectly_true() {
+      Squad mSquad = new Squad("Wazito");
+    assertEquals(true, mSquad instanceof Squad);
+    }
+
 }
