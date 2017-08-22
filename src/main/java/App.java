a7fa6c7559a31fef676f03b94e73165d17da4107
EMichira <emmanuelmichira@gmail.com>
2017-08-22 19:34:02 +0300

    Added route that processes form submission and creates new heroes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -53,5 +53,20 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    post("/heroes", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+
+      Squad squad = Squad.find(Integer.parseInt(request.queryParams("squadId")));
+
+      String name = request.queryParams("name");
+      Hero newhero = new Hero(name);
+
+      squad.addHero(newhero);
+
+      model.put("squad", squad);
+      model.put("template", "templates/squad-heroes-success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
     }
 }
