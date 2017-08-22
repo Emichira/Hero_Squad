189f4d782eba941d5401576106e2d2013812137b
EMichira <emmanuelmichira@gmail.com>
2017-08-22 12:04:26 +0300

    Added route to render our particular squads page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -37,5 +37,13 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/squads/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Squad squad = Squad.find(Integer.parseInt(request.params(":id")));
+      model.put("squad", squad);
+      model.put("template", "templates/squad.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
     }
 }
