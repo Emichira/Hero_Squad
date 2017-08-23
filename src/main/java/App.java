092f5e68c63f0425f2c99131f801ba60bf0702da
EMichira <emmanuelmichira@gmail.com>
2017-08-23 19:56:00 +0300

    Added new routes for rendering hero onjects

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -53,6 +53,38 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("heroes/new", (request, response) -> {
+     Map<String, Object> model = new HashMap<String, Object>();
+     model.put("template", "templates/hero-form.vtl");
+     return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/heroes", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("heroes", Hero.all());
+      model.put("template", "templates/heroes.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/heroes", (request,response) -> {
+    Map<String, Object> model = new HashMap<String, Object>();
+    String name = request.queryParams("name");
+    int age = Integer.parseInt(request.queryParams("age"));
+    String superpower = request.queryParams("superpower");
+    String weakness = request.queryParams("weakness");
+    Hero userHero = new Hero(name, age, superpower, weakness);
+    model.put("template", "templates/success.vtl");
+    return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/heroes/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Hero hero = Hero.find(Integer.parseInt(request.params(":id")));
+      model.put("heroes", hero);
+      model.put("template", "templates/hero.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
     post("/heroes", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       Squad squad = Squad.find(Integer.parseInt(request.queryParams("squadId")));
@@ -66,6 +98,23 @@ public class App {
       model.put("template", "templates/squad-heroes-success.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
+
+  //   post("/heroes", (request, response) -> {
+  //   Map<String, Object> model = new HashMap<String, Object>();
+   //
+  //   ArrayList<Hero> heroes = request.session().attribute("heroes");
+  //   if (heroes == null) {
+  //     heroes = new ArrayList<Hero>();
+  //     request.session().attribute("heroes", heroes);
+  //   }
+   //
+  //   String description = request.queryParams("squadId");
+  //   Hero newHero = new Hero(description);
+  //   heroes.add(newHero);
+   //
+  //   model.put("template", "templates/success.vtl");
+  //   return new ModelAndView(model, layout);
+  //  }, new VelocityTemplateEngine());
 
     }
 }
